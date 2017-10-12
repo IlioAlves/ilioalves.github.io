b8de140d187e9af7a48e6d66f8d9e0634933151d
Ilio Alves <ilioalve@gmail.com>
2017-10-12 15:51:43 -0300

    arrumando a animaçao

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var typed2 = new Typed('#typing', {
-	strings: ['Desenvolvedor', 'Matemático', 'Determinado'],
-	typeSpeed: 50,
-	backSpeed: 50,
-	fadeOut: true,
-	loop: true
+    strings: ['Desenvolvedor', 'Matemático', 'Determinado'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
 });
