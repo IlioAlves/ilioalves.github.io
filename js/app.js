87f3b16e922c0e05268d196868c5c31aabbc8787
Ilio Alves <ilioalve@gmail.com>
2017-10-18 15:44:43 -0200

    fezendo o formulario

--- js/app.js
+++ js/app.js
@@ -4,3 +4,18 @@ var typed2 = new Typed('#typing', {
     backSpeed: 100,
     loop: true
 });
+
+$('#form-contact').on('submit', function(e) {
+    e.preventDefault();
+    emailjs.init("user_EpzwHVHyCg805uQEvE6mS");
+    emailjs.send("sendgrid", "contact",{
+      name: $('#input-name').val(),
+      email: $('#input-email').val(),
+      subject: $('#input-subject').val(),
+      message: $('#input-message').text()
+    }).then(function(response) {
+       swal ( "Email enviado com sucesso");
+    }, function(err) {
+       swal ( "Oops" , err ,  "error" );
+    });
+});
